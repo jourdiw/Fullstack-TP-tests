faf888e1afbac4c351a96b442cdb62b69f2ab7ae
Jourdi Waller <jourdiwaller@gmail.com>
2019-02-21 14:18:37 +0100

    Corrected test and snapshots

--- src/test/Home.spec.js
+++ src/test/Home.spec.js
@@ -1,15 +1,29 @@
-import { shallowMount } from '@vue/test-utils'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import VueRouter from 'vue-router';
+import { shallowMount } from '@vue/test-utils';
+import Home from '../views/Home.vue';
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
 
 describe('Home.vue', () => {
   it('button is in a router link', () => {
     const wrapper = shallowMount(Home, {
       mocks: {
-        $t: () => {}
-      }
+        $t: () => {},
+      },
     });
     // expect(wrapper);
     // expect(wrapper.find('v-btn'));
     expect(wrapper.find('router-link'));
+  });
+  test('renders correctly', () => {
+    const wrapper = shallowMount(Home, {
+      mocks: {
+        $t: () => {},
+      },
+    });
+    expect(wrapper.element).toMatchSnapshot();
   });
 });
