67f7a9ebfa01b8f25b407eca0a586ee3d3d4b88c
Jourdi Waller <jourdiwaller@gmail.com>
2019-02-25 16:25:25 +0100

    test(Cypress): Starting to write tests. Bug to be fixed on JSX rendering

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,10 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure } from '@storybook/vue'
+import { configure } from '@storybook/vue';
+import '../../src/plugins/vuetify';
+import { addDecorator } from '@storybook/vue/dist/client/preview';
 
-const req = require.context('../../src/stories', true, /.stories.js$/)
+addDecorator(() => (
+  { template: '<v-app><story /></v-app>' }
+));
+
+const req = require.context('../../src', true, /.stories.js$/);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,9 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
+// import Vue from 'vue';
+// import Vuetify from 'vuetify';
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import MyButton from '../components/MyButton.vue';
+import MyAddress from '../components/Address/Address.vue';
+
+// Vue.use(Vuetify);
 
 storiesOf('Button', module)
   .add('with text', () => ({
@@ -21,5 +26,48 @@ storiesOf('Button', module)
   .add('with some emoji', () => ({
     components: { MyButton },
     template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+    methods: { action: action('clicked') },
+  }));
+
+storiesOf('Address', module)
+  .add('with text', () => ({
+    components: { MyAddress },
+    template: `<my-address
+      :address="{
+        complement: '1',
+        road: 'road',
+        zipCode: '90015',
+        city: 'city',
+        state: 'state',
+      }" label="My Label"></my-address>`,
+    methods: { action: action('clicked') },
+  }))
+  .add('with JSX', () => ({
+    components: { MyAddress },
+    props: {
+      address: {
+        complement: '1',
+        road: 'road',
+        zipCode: '90015',
+        city: 'city',
+        state: 'state',
+      },
+      label: 'My Label',
+    },
+    render() {
+      // const { address, label } = this.data;
+      return <my-address address="{
+        complement: '1',
+        road: 'road',
+        zipCode: '90015',
+        city: 'city',
+        state: 'state',
+      }"/>;
+    },
+    methods: { action: linkTo('Button', 'with some emoji') },
+  }))
+  .add('with some emoji', () => ({
+    components: { MyAddress },
+    template: '<address>Hello address</address>',
     methods: { action: action('clicked') },
   }));
